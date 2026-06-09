c60f44b65a64fec2224418142b031620a370f000
Jaykisan Zende <zendeakshay16@gmail.com>
2026-06-09 15:22:21 +0530

    Reactive Form Validation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginFormComponent } from './pages/login-form/login-form.component';
 import { ErrorPageComponent } from './pages/error-page/error-page.component';
+import { ReactiveFormComponent } from './pages/reactive-form/reactive-form.component';
 
 const routes: Routes = [
   {path : '', redirectTo:'login', pathMatch:'full'},
   {path : 'login' , component:LoginFormComponent},
+  {path : 'reactive-form' , component:ReactiveFormComponent},
   {path:'**' , component:ErrorPageComponent}
 ];
 
--- src/app/pages/reactive-form/reactive-form.component.ts
+++ src/app/pages/reactive-form/reactive-form.component.ts
@@ -1,10 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AbstractControl, FormControl, FormGroup, ValidationErrors, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-reactive-form',
   templateUrl: './reactive-form.component.html',
   styleUrls: ['./reactive-form.component.scss']
 })
-export class ReactiveFormComponent {
+export class ReactiveFormComponent implements OnInit {
+
+  empForm: FormGroup = new FormGroup({
+    name: new FormControl('', [Validators.required, Validators.pattern('^[a-zA-Z]+$')]),
+    email: new FormControl('', [Validators.required, Validators.email]),
+    skill: new FormControl('', [Validators.required,this.skillValidator]),
+    salary: new FormControl('', [Validators.minLength(5), Validators.maxLength(8)])
+  })
+
+  skillValidator(control:AbstractControl):ValidationErrors | null{
+    const skill = control.value;
+    if(skill !== 'Angular'){
+      return {invalidSkill : true};
+    }
+    return null;
+  }
+  constructor() {
+  }
+
+  ngOnInit(): void {
+    this.empForm.patchValue({
+      name: 'Jay',
+      email: 'jay@gmail.com',
+      skill: 'Angular',
+      salary: '234567'
+    })
+  }
+
+  submit() {
+    console.log(this.empForm.value);
+
+  }
 
 }

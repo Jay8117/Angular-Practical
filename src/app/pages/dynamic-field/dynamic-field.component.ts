31bd74714b1591b5ba511bb8733bfb09912db29a
Jaykisan Zende <zendeakshay16@gmail.com>
2026-06-11 11:38:45 +0530

    Dynamic field add functionality

--- src/app/pages/dynamic-field/dynamic-field.component.ts
+++ src/app/pages/dynamic-field/dynamic-field.component.ts
@@ -1,10 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormArray, FormBuilder, FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-dynamic-field',
   templateUrl: './dynamic-field.component.html',
   styleUrls: ['./dynamic-field.component.scss']
 })
-export class DynamicFieldComponent {
+export class DynamicFieldComponent implements OnInit {
 
+  userForm! : FormGroup;
+  userData :any = [];
+  constructor(private fb:FormBuilder){
+    this.userForm = this.fb.group({
+      name:new FormControl(''),
+      skills : this.fb.array([
+        this.fb.control('')
+      ]
+      )
+    })
+  }
+
+  get skills():FormArray{
+    return this.userForm.get('skills') as FormArray
+  }
+
+  ngOnInit(): void {
+    
+  }
+  addSkill(){
+    this.skills.push(
+      this.fb.control('')
+    )
+  }
+  removeSkill(i:number){
+    this.skills.removeAt(i)
+  }
+
+  getData(){
+    console.log(this.userForm.value);
+    this.userData = [];
+    this.userData.push(this.userForm.value)
+    console.log(this.userData);
+  }
 }
